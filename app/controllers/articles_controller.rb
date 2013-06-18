56f14fe915c0b8219a8840888ccc02448c81e4b9
levelone <marc17seifert@gmail.com>
2013-06-18 20:26:03 +0800

    comment fixed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 	def index
     @articles = Article.order('created_at DESC')
-    @comments = Comment.new
+    @comments = Comment.order('created_at DESC')
 	end
 
 	def edit
